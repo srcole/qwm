3684086370abac3b5c704e48c3b81e227fb3b7b8
Scott Cole <scott.cole0@gmail.com>
2017-10-01 10:46:48 -0400

    update retirement bokeh

--- retire/make_bokeh.py
+++ retire/make_bokeh.py
@@ -10,10 +10,10 @@ N_years = 50
 # Slider variables
 annual_ROI = .06
 annual_inflation = .03
-annual_savings = 10
+annual_savings = 10000
 retired_tax_rate = .2
-savings_start = 10
-retired_income = 35
+savings_start = 10000
+retired_income = 35000
 
 # Compute savings over time
 x = np.arange(N_years)
@@ -25,7 +25,8 @@ for i in range(1, N_years):
     y_todays_dollar.append(y_todays_dollar[-1] * (1 + annual_inflation))
     y_interest.append(
         (y_savings[-1] * annual_ROI * (1 - retired_tax_rate)) / y_todays_dollar[-1])
-    y_savings.append((y_savings[-1] * (1 + annual_ROI) + annual_savings * y_todays_dollar[i]))
+    y_savings.append(
+        (y_savings[-1] * (1 + annual_ROI) + annual_savings * y_todays_dollar[i]))
     if y_interest[-1] >= retired_income and retire_year == 0:
         retire_year = i
 
@@ -42,11 +43,14 @@ source = ColumnDataSource(
 # Make initial figure of net income vs years of saving
 plot = figure(plot_width=400, plot_height=400,
               x_axis_label='Years from start of savings',
-              y_axis_label='Annual net income on interest (1000\'s of today\'s $)',
+              y_axis_label='Annual net income on interest ($)',
               x_range=(0, N_years))
-r1 = plot.line('x', 'y', source=source, line_width=3, line_alpha=0.6, line_color='black', legend='income on interest')
-r2 = plot.line('x', 'y_ri', source=source, line_width=3, line_alpha=0.6, line_color='red', legend='retirement threshold')
-r3 = plot.line('x_rt', 'y_rt', source=source, line_width=3, line_alpha=0.6, line_color='green', legend='year of retirement')
+r1 = plot.line('x', 'y', source=source, line_width=3,
+               line_alpha=0.6, line_color='black', legend='annual income on interest')
+r2 = plot.line('x', 'y_ri', source=source, line_width=3,
+               line_alpha=0.6, line_color='red', legend='retirement income threshold')
+r3 = plot.line('x_rt', 'y_rt', source=source, line_width=3,
+               line_alpha=0.6, line_color='green', legend='year of retirement')
 plot.legend.location = 'top_left'
 plot.legend.label_text_font_size = '10pt'
 
@@ -98,18 +102,18 @@ callback = CustomJS(args=dict(source=source), code="""
 """)
 
 # Define all sliders
-savings_start_slider = Slider(start=0, end=100, value=savings_start, step=1,
-                              title="Starting savings (1000\'s of today\'s $)", callback=callback)
-annual_savings_slider = Slider(start=0, end=50, value=annual_savings, step=1,
-                               title="Annual savings (1000\'s of today\'s $)", callback=callback)
+savings_start_slider = Slider(start=0, end=100000, value=savings_start, step=1000,
+                              title="Starting savings ($)", callback=callback)
+annual_savings_slider = Slider(start=0, end=50000, value=annual_savings, step=1000,
+                               title="Annual savings ($)", callback=callback)
+retired_income_slider = Slider(start=0, end=100000, value=retired_income, step=1000,
+                               title="Retired net income ($)", callback=callback)
 annual_ROI_slider = Slider(start=0, end=.2, value=annual_ROI, step=.01,
                            title="Annual ROI", callback=callback)
 annual_inflation_slider = Slider(start=0, end=.05, value=annual_inflation, step=.001,
                                  title="Annual inflation", callback=callback)
 retired_tax_rate_slider = Slider(start=0, end=.5, value=retired_tax_rate, step=.01,
                                  title="Tax rate (when retired)", callback=callback)
-retired_income_slider = Slider(start=10, end=100, value=retired_income, step=1,
-                               title="Retired net income (1000\'s of today\'s $)", callback=callback)
 
 # Define which parameter each slider refers to
 callback.args["savings_start"] = savings_start_slider
@@ -122,10 +126,11 @@ callback.args["retired_income"] = retired_income_slider
 # Define layout of plot and sliders
 layout = row(
     plot,
-    widgetbox(savings_start_slider, annual_ROI_slider, annual_savings_slider,
-              annual_inflation_slider, retired_tax_rate_slider, retired_income_slider),
+    widgetbox(savings_start_slider, annual_savings_slider, retired_income_slider,
+              annual_ROI_slider, annual_inflation_slider, retired_tax_rate_slider),
 )
 
 # Output and show
-output_file("/gh/srcole.github.io/assets/misc/retirement_bokeh.html", title="Retirement calculator")
+output_file("/gh/srcole.github.io/assets/misc/retirement_bokeh.html",
+            title="Retirement calculator")
 show(layout)
